14c8f33ab51287b2d47377e88de9c77e259e8b2e
ibrahim adel abdelaziz <Ibrahimadel2810@yahoo.com>
2025-08-08 22:06:59 +0300

    remove funcation and marking

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
     const textEl = document.getElementById("item");
     const listEl = document.getElementById("list");
     let items=[]
+  
 
-    function addItem() {
+    function addItem() 
+    {
         let item = textEl.value.trim();
+     
 
         
-        if (item === null) return; // skip empty input
+        if (item ==="") return; 
 
 
         items.push(item);
-        listEl.innerHTML += `
+        renderItems();
+        
+  
+
+        textEl.value = "";
+
+    }
+
+    function renderItems()
+    {
+        listEl.innerHTML = "";
+        for(let i=0;i<items.length;i++)
+        {
+            listEl.innerHTML += `
             <div>
                 <ul>
                 <li>
-                <input type="checkbox">
-                <label>${item}</label>
+                <input type="checkbox" onchange="toggleChecked(this)">
+                <label>${items[i]}</label>
+                <button onclick="removeItem(${i})" onchange="toggleChecked(this)">x</button>
                 </li>
                 </ul>
             </div>
         `;
-  
-
-        textEl.value = ""; // clear the input
-
+        }
     }
+    function removeItem(index)
+    {
+        items.splice(index, 1); // remove 1 element at 'index'
+        renderItems(); // refresh list
+    }
+    function toggleChecked(checkbox) {
+    const label = checkbox.nextElementSibling; // label is right after the checkbox
+    if (checkbox.checked) {
+        label.classList.add("checked");
+    } else {
+        label.classList.remove("checked");
+    }
+}
